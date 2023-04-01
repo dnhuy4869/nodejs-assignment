5ab1867ee8266c40b9733a4e8a6390b9f3093a18
Huy <73681373+dnhuy4869@users.noreply.github.com>
2023-04-01 16:28:05 +0700

    product api

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,4 +1,5 @@
-const Product = require("../models/product")
+const Product = require("../models/product");
+const Category = require("../models/category");
 
 const getAll = async (req, res) => {
     try {
@@ -14,6 +15,100 @@ const getAll = async (req, res) => {
     }
 }
 
+const createOne = async (req, res) => {
+    try {
+        if (!req.body.idCategory)
+        {
+            return res.status(404).json({
+                message: "Category is not exist",
+            })
+        }
+
+        const cate = await Category.findById(req.body.idCategory);
+        if (!cate) {
+            return res.status(404).json({
+                message: "Category is not exist",
+            })
+        }
+
+        const newProduct = await new Product(req.body);
+        await newProduct.save();
+
+        return res.status(200).json({
+            message: "Created successfully"
+        })
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: "Internal server error",
+            error: err,
+        })
+    }
+}
+
+const updateOne = async (req, res) => {
+    try {
+        if (!req.body.idCategory)
+        {
+            return res.status(404).json({
+                message: "Category is not exist",
+            })
+        }
+
+        const cate = await Category.findById(req.body.idCategory);
+        if (!cate) {
+            return res.status(404).json({
+                message: "Category is not exist",
+            })
+        }
+
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(403).json({
+                message: "Product is not exist",
+            })
+        }
+
+        await product.updateOne(req.body);
+
+        return res.status(200).json({
+            message: "Updated successfully",
+        })
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: "Internal server error",
+            error: err,
+        })
+    }
+}
+
+const deleteOne = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(403).json({
+                message: "Product is not exist",
+            })
+        }
+
+        await product.deleteOne();
+
+        return res.status(200).json({
+            message: "Deleted successfully",
+        });
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: "Internal server error",
+            error: err,
+        })
+    }
+}
+
 module.exports = {
     getAll,
+    createOne,
+    updateOne,
+    deleteOne
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -4,4 +4,10 @@ const router = require("express").Router();
 
 router.get("/get-all", productController.getAll);
 
+router.post("/create-one", productController.createOne);
+
+router.put("/update-one/:id", productController.updateOne);
+
+router.delete("/delete-one/:id", productController.deleteOne);
+
 module.exports = router;
